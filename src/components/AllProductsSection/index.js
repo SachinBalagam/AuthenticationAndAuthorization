2d11ccde672bde95cba4b3577bdafcda7c3d22a3
SachinBalagam <balagamsachin337@gmail.com>
2023-09-27 05:33:12 +0000

    Final Commit

--- src/components/AllProductsSection/index.js
+++ src/components/AllProductsSection/index.js
@@ -1,4 +1,6 @@
+import Cookies from 'js-cookie'
 import {Component} from 'react'
+import Loader from 'react-loader-spinner'
 
 import ProductCard from '../ProductCard'
 import './index.css'
@@ -6,18 +8,58 @@ import './index.css'
 class AllProductsSection extends Component {
   state = {
     productsList: [],
+    isLoading: true,
+  }
+
+  componentDidMount() {
+    this.getProducts()
+  }
+
+  getProducts = async () => {
+    const apiUrl = 'https://apis.ccbp.in/products'
+    const jwtToken = Cookies.get('jwt_token')
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.products.map(eachProduct => ({
+        id: eachProduct.id,
+        brand: eachProduct.brand,
+        title: eachProduct.title,
+        price: eachProduct.price,
+        rating: eachProduct.rating,
+        imageUrl: eachProduct.image_url,
+      }))
+      console.log(updatedData)
+      this.setState({productsList: updatedData, isLoading: false})
+    }
   }
 
   renderProductsList = () => {
-    const {productsList} = this.state
+    const {productsList, isLoading} = this.state
     return (
       <div>
         <h1 className="products-list-heading">All Products</h1>
-        <ul className="products-list">
-          {productsList.map(product => (
-            <ProductCard productData={product} key={product.id} />
-          ))}
-        </ul>
+        {isLoading ? (
+          <Loader
+            type="TailSpin"
+            color="#00BFFF"
+            height={50}
+            width={50}
+            className="products-loader-container"
+          />
+        ) : (
+          <ul className="products-list">
+            {productsList.map(product => (
+              <ProductCard productData={product} key={product.id} />
+            ))}
+          </ul>
+        )}
       </div>
     )
   }
